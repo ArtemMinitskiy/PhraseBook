6410fffe0066fcafda161c8e1e630ee034e07da6
rukok <rukokuro@gmail.com>
2019-06-26 20:22:19 +0200

    Add Account Detail and small fixes

--- app/src/main/java/com/example/artem/phrasebook/MainActivity.java
+++ app/src/main/java/com/example/artem/phrasebook/MainActivity.java
@@ -13,14 +13,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.artem.phrasebook.AlertDialog.AlertDialogPhrase;
 import com.example.artem.phrasebook.AlertDialog.AlertDialogSE;
 import com.example.artem.phrasebook.AlertDialog.AlertDialogWord;
 import com.example.artem.phrasebook.Fragment.DictionaryFragment;
 import com.example.artem.phrasebook.Fragment.PhraseBookThemeFragment;
 import com.example.artem.phrasebook.Fragment.TabFragment;
+import com.example.artem.phrasebook.Model.Users;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -32,13 +42,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     NavigationView mNavigationView;
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+
     private FragmentManager mFragmentManager;
     private FragmentTransaction mFragmentTransaction;
+
     DialogFragment dialogWord;
     DialogFragment dialogPhrase;
     DialogFragment dialogSE;
-    FirebaseAuth mAuth;
-    FirebaseAuth.AuthStateListener mAuthListner;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListner;
+    private FirebaseUser mFirebaseUser;
+    private DatabaseReference mRef;
+
+    private TextView userEmail, userFullName;
+    private ImageView userImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,20 +66,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ButterKnife.bind(this);
 
         mAuth = FirebaseAuth.getInstance();
-
-        mAuthListner = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                if (firebaseAuth.getCurrentUser()==null)
-                {
-                    startActivity(new Intent(MainActivity.this, SignInActivity.class));
-                }
-            }
-        };
+        mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        mRef = FirebaseDatabase.getInstance().getReference();
 
         dialogWord = new AlertDialogWord();
         dialogPhrase = new AlertDialogPhrase();
         dialogSE = new AlertDialogSE();
+
+        View navHeaderView = mNavigationView.getHeaderView(0);
+
+        userEmail = (TextView) navHeaderView.findViewById(R.id.userEmail);
+        userFullName = (TextView) navHeaderView.findViewById(R.id.userName);
+        userImage = (ImageView) navHeaderView.findViewById(R.id.userImageView);
+
+        mAuthListner = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() != null) {
+                    getAccountDetails();
+                }
+                else {
+                    startActivity(new Intent(MainActivity.this, SignInActivity.class));
+                }
+            }
+        };
 
         mFragmentManager = getSupportFragmentManager();
         mFragmentTransaction = mFragmentManager.beginTransaction();
@@ -97,6 +126,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mDrawerLayout.setDrawerListener(mDrawerToggle);
         mDrawerToggle.syncState();
 
+    }
+
+    private void getAccountDetails() {
+        FirebaseDatabase.getInstance().getReference("Users").child(mFirebaseUser.getEmail().replace(".", ","))
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Users users = dataSnapshot.getValue(Users.class);
+                        Glide.with(MainActivity.this).load(mFirebaseUser.getPhotoUrl()).into(userImage);
+                        userFullName.setText(users.getUser());
+                        userEmail.setText(users.getEmail());
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
     }
 
 
--- app/src/main/java/com/example/artem/phrasebook/SignInActivity.java
+++ app/src/main/java/com/example/artem/phrasebook/SignInActivity.java
@@ -13,6 +13,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.artem.phrasebook.Model.Users;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -26,6 +27,9 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -40,13 +44,6 @@ public class SignInActivity extends AppCompatActivity {
     private final static int RC_SIGN_IN = 123;
     GoogleSignInClient mGoogleSignInClient;
     FirebaseAuth.AuthStateListener mAuthListner;
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListner);
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -155,6 +152,13 @@ public class SignInActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListner);
+
+    }
+
+    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
@@ -174,16 +178,28 @@ public class SignInActivity extends AppCompatActivity {
         }
     }
 
-    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+    private void firebaseAuthWithGoogle(final GoogleSignInAccount account) {
         AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithCredential:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
+//                            FirebaseUser user = mAuth.getCurrentUser();
+                            Users user = new Users(
+                                    account.getDisplayName(),
+                                    account.getEmail(),
+                                    account.getPhotoUrl().toString(),
+                                    FirebaseAuth.getInstance().getCurrentUser().getUid()
+                            );
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            DatabaseReference userRef = database.getReference("Users");
+                            userRef.child(account.getEmail().replace(".", ",")).setValue(user, new DatabaseReference.CompletionListener() {
+                                @Override
+                                public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                                }
+                            });
                             //updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
